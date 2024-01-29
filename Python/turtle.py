612d480cca10ad629b049626b6d1945204c893ee
noascape <152854295+noascape@users.noreply.github.com>
2024-01-29 10:43:25 +0100

    order established

--- Python/turtle.py
+++ Python/turtle.py
@@ -1,28 +1,14 @@
+import turtle 
+wn = turtle.Screen()
+wn.bgcolor("light green")
+skk = turtle.Turtle()
+skk.color("blue")
 
-import turtle
-size = 8
-length = 50
-startx = -200
-starty = -200
-def nestedloop():
+def sqrfunc(size):
+    for i in range(4):
+        skk.fd(size)
+        skk.left(90)
+        size = size + 5
 
- turtle.speed(0)
-
- for row1 in range(size):
-     for col1 in range(size):
-        if (row1+col1)%2 == 0:
-             turtle.fillcolor("black")
-        else:
-            turtle.fillcolor("blue")
-            turtle.begin_fill()
-            a = startx + col1*length
-            b = starty + row1*length
-            turtle.penup()
-            turtle.setpos(a,b)
-            turtle.pendown()
-            for i in range(4):
-                     turtle.forward(length)
-                     turtle.left(90)
-                     turtle.end_fill()
-nestedloop()
-turtle.done()
+for j in range(6,146,20):
+    sqrfunc(j)
