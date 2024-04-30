eb388ec5a99209d05075453f2e77c914a7b77bd2
noascape <152854295+noascape@users.noreply.github.com>
2024-04-30 10:11:44 +0200

    Update script.js

--- Web/No wifi game/script.js
+++ Web/No wifi game/script.js
@@ -1,5 +1,6 @@
 var character = document.getElementById('character');
 var block = document.getElementById('block');
+
 function jump() {
     if(character.classList != "animate"){
     character.classList.add("animate");
