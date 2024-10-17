cf9eed977bd9956a1c0b41257c3c5f647acc908d
noascape <152854295+noascape@users.noreply.github.com>
2024-10-17 11:48:50 +0200

    new testmanagement

---
+++ Java/02_Test/src/main/java/model/EngineState.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum EngineState {
+    ACTIVE, INACTIVE, MAINTENANCE, FAILED
+}
---
+++ Java/02_Test/src/main/java/model/Manufacturer.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Manufacturer {
+    GE, ROLLS_ROYCE, PRATT_WHITNEY, SAFRAN
+}
