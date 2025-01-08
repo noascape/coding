bf26b971e604be18883bda3a0cb18cb489f1c665
noascape <nheim200@gmail.com>
2025-01-08 16:11:43 +0100

    .

--- Python/statistic/median.py
+++ Python/statistic/median.py
@@ -1,6 +1,6 @@
 import csv
 
 with open('Python/statistic/state.csv', newline='') as csvfile: 
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')        #delimiter = Trennzeichen von Spalten  | quotechar gibt an, welches Zeichen verwendet wird um Inhalte einzuschließen
     for row in spamreader:
-        print(', '.join(row))
+        print(', '.join(row))     #.join verbindet Elemente eines iterierbaren Objekts zu einem String
