08d80aed12ac23f7ad6a4c4ede3155f35243f839
noascape <nheim200@gmail.com>
2025-01-14 13:21:17 +0100

    .

---
+++ Web/Projekte/Tech_Webseite/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dsn = 'mysql:dbname=userdb;host=localhost';  
+    $username = 'root';                           
+    $password = '';                              
+
+    $con = new PDO($dsn, $username, $password);   
+
+?>
