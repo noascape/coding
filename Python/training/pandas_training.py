8923784f04ab9dab69dd76904d020bf7339374e7
paheimno <noah.heim@de.ebmpapst.com>
2025-05-06 17:51:22 +0200

    improved python training

---
+++ Python/training/pandas_training.py
@@ -0,0 +1,8 @@
+#Pandas-Bibliothek: verwendet C-Erweiterungen (meist in Cyphon geschrieben), um bestimmte Operationen zu beschleunigen
+# Dokumentation aller Funktionen: https://pandas.pydata.org/docs/reference/
+
+import pandas as pd
+
+
+
+df = pd.DataFrame()
--- Python/training/types_training.py
+++ Python/training/types_training.py
@@ -2,6 +2,7 @@
 friends = ["Kevin", "Karen", "Jim"]                                             #friend array
 friends_extention = ["Rob", "Will"]
 lucky_numbers = [4, 8, 15, 17, 33, 43]                                          #number array
+
 friends2 = friends.copy()                                                       #copy a list into another list
 friends[0] = "Mike"                                                             #modify the list
 friends.extend(friends_extention)                                               #extend one list with another one
@@ -11,9 +12,20 @@ lucky_numbers.pop()                                                             
 friends.count("Jim")                                                            #count the amount of Jim`s in the list
 lucky_numbers.reverse()                                                         #reverse the order of the list
 print(friends[1])                                                               #list output of the element with the array 1 (=Karen), you could also do it from the right side of the list [-1] = Jim
+friends.remove("Kevin")
+friends.index("Karen")
+lucky_numbers.sort()
+friends2.clear()
+sorted(friends)
+umgekehrt = friends[::-1]                                                       #gibt eine umgekehrte Kopie der Liste
+len(umgekehrt)
+#in                                                                             #prüft, ob ein Element enthalten ist: if "Jim" in friends:
+
+
 
 #Tuple ()
 coordinates = (4, 5)                                                            #a Tuple ist like a list, but (), cannot be modified
+
 
 #Dictionary {} (dict)
 monthConversions = {
@@ -32,6 +44,17 @@ monthConversions = {
 }
 print(monthConversions["Nov"])                                                  #will give the associated value of the key
 print(monthConversions.get("Luc", "Not a valid Key"))                           #you can specify a default value in case the key is not faund in the dictinoary
+monthConversions.keys()
+monthConversions.values()
+monthConversions.items()
+monthConversions.update({"Dec": "Dezember"})
+monthConversions.update(Dec="December")
+del monthConversions["Dec"]
+
+
+# SET                                                                          #Verwendbar für Mengenoperationen (union, intersection, difference), Duplikat-Erkennung oder -Entfernung
+my_set = {1, 2, 3, 2}
+print(my_set)
 
 #2D List
 number_grid = [
