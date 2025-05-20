46a33eb0dbf017b46f9f5a0cdddb9ddb62e439ca
paheimno <noah.heim@de.ebmpapst.com>
2025-05-20 17:35:36 +0200

    types improved

--- Python/training/types_training.py
+++ Python/training/types_training.py
@@ -1,64 +1,158 @@
-#Array/List [] (list)
 from typing import Tuple
 
-friends = ["Kevin", "Karen", "Jim"]                                             #friend array
-friends_extention = ["Rob", "Will"]
-lucky_numbers = [4, 8, 15, 17, 33, 43]                                          #number array
 
-friends2 = friends.copy()                                                       #copy a list into another list
-friends[0] = "Mike"                                                             #modify the list
-friends.extend(friends_extention)                                               #extend one list with another one
-friends.append("Luke")                                                          #append a value to the end of the list
-lucky_numbers.insert(1, 8)                                       #insert a value to a position and the other values will be pushed to the right
-lucky_numbers.pop()                                                             #to remove the last value, you can also do .clear or .remove and some more
-friends.count("Jim")                                                            #count the amount of Jim`s in the list
-lucky_numbers.reverse()                                                         #reverse the order of the list
-print(friends[1])                                                               #list output of the element with the array 1 (=Karen), you could also do it from the right side of the list [-1] = Jim
-friends.remove("Mike")
-friends.index("Karen")
-lucky_numbers.sort()
-friends2.clear()
-sorted(friends)
-letzes_element = friends[-1]
-umgekehrt = friends[::-1]                                                       #gibt eine umgekehrte Kopie der Liste
-len(umgekehrt)
-#in                                                                             #prüft, ob ein Element enthalten ist: if "Jim" in friends:
+"""
+ ----- Array/List [] (list) -----
+"""
+friends = ["Kevin", "Karen", "Jim"]
+friends_ext = ["Rob", "Will"]
+
+# Kopieren
+friends2 = friends.copy()
+
+# Hinzufügen
+friends.append("Luke")          # Einzelwert
+friends.extend(friends_ext)     # Iterable („aufklappen“)
+friends.insert(1, "Olaf")       # an Index 1
+
+# Entfernen
+friends.pop()                   # letztes Element
+friends.pop(1)                  # Element an Index 1
+friends.remove("Jim")           # erstes Vorkommen eines Werts
+friends.clear()                 # alle Elemente löschen
+
+# Suchen & Zählen
+idx = friends.index("Karen")    # Position oder ValueError
+cnt = friends.count("Jim")      # Anzahl Vorkommen
+
+# Sortieren & Umkehren
+friends.sort()                  # in-place sortieren
+friends.reverse()               # Reihenfolge umdrehen
+
+# Slicing kopiert Teilabschnitte
+teil = friends[1:4]             # Elemente Index 1 bis 3
+schritte = friends[::2]         # jedes zweite Element
+
+# List Comprehension (Filter + Transform)
+nums = [1, 2, 3, 4, 5]
+squares = [x*x for x in nums if x % 2 == 1]  # [1,9,25]
 
 
 
-#Tuple ()
-coordinates = (4, "Anna", 5)                                                #a Tuple ist like a list(Array), but (), cannot be modified, can have elements of different types  --> return num1, name, num2
+
+"""
+ ----- Tuple () -----
+"""
+coordinates = (4, "Anna", 5)
+
+# Zugriff
+x, name, y = coordinates    # Unpacking
+first = coordinates[0]
+
+# Methoden (nur zwei, da unveränderlich)
+idx1 = coordinates.index("Anna")
+cnt1 = coordinates.count(5)  # 5 kommt "1"mal vor
 
 def tuple_function() -> Tuple[int, str, int]:
-    return coordinates                                                      #the position will always remain the same -> coordinates[1] is always the name in this case
+    return coordinates                 #immer gleiche Position -> coordinates[1] ist immer der name in diesem Fall
 
 
-#Dictionary {} (dict) (= Hash-Map)
+
+
+"""
+ ----- Dictionary {} (dict) (= Hash-Map) -----
+"""
 monthConversions = {
-    "Jan": "January",                                                          #on the left a unique key in the dicitionary, on the ride the value
+    "Jan": "January",
     "Feb": "February",
     "Mar": "March",
-    "Apr": "April",
-    "May": "May",
-    "Jun": "June",
-    "Jul": "July",
-    "Aug": "August",
-    "Sep": "September",
-    "Oct": "October",
-    "Nov": "November",
-    "Dec": "December",
 }
-print(monthConversions["Nov"])                                                  #will give the associated value of the key
-print(monthConversions.get("Luc", "Not a valid Key"))                           #you can specify a default value in case the key is not faund in the dictinoary
-monthConversions.keys()
-monthConversions.values()
-monthConversions.items()
-monthConversions.update({"Dec": "Dezember"})
-monthConversions.update(Dec="December")
-del monthConversions["Dec"]
+
+# Zugriff
+val = monthConversions["Jan"]            # KeyError, wenn nicht da
+val1 = monthConversions.get("Luc", "–")   # Default
+
+# Hinzufügen / Überschreiben
+monthConversions["Apr"] = "April"
+monthConversions.update({"May": "May", "Jun": "June"})
+monthConversions.update(Jul="July")      # Kurzform
+
+# Entfernen
+monthConversions.pop("Mar")              # gibt Wert zurück oder Fehler
+monthConversions.pop("Foo", None)        # sicherer Default
+monthConversions.popitem()               # entfernt ein beliebiges Paar
+del monthConversions["Feb"]
+monthConversions.clear()
+
+# Iterieren
+for k in monthConversions:               # über Keys
+    print(k, monthConversions[k])
+for k, v in monthConversions.items():    # Key/Value-Paare
+    print(k, v)
+
+# Nützliche Klassenmethoden
+new_dict = dict.fromkeys(["a","b","c"], 0)  # {'a':0,'b':0,'c':0}
+
+# setdefault: liefert vorhandenen Wert oder setzt Default
+x = monthConversions.setdefault("Aug", "August")
+
+# Dict-Comprehension
+full_months = ["January","February","March"]
+abbreviation = { m[:3]: m for m in full_months }
 
 
-# Beispiele: JSON-Datein:
+
+
+"""
+ ----- Mengen / set() -----
+"""
+s = {1, 2, 3, 2}    # Duplikate werden automatisch entfernt
+
+# Hinzufügen / Entfernen
+s.add(4)
+s.update([4,5,6])   # wie extend für Listen
+s.discard(10)       # kein Fehler, wenn nicht da
+s.remove(3)         # KeyError, wenn nicht da
+
+# Mengenoperationen
+a = {1,2,3}
+b = {3,4,5}
+print(a | b)        # Vereinigung
+print(a & b)        # Schnittmenge
+print(a - b)        # Differenz
+print(a ^ b)        # symmetrische Differenz
+
+# Abfragen
+exists = 2 in s
+s.clear()
+
+
+
+
+"""
+ ----- Zweidimensionale Listen & Matrix-Zugriff -----
+"""
+grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+# Zugriff
+print(grid[1][2])   # Zeile 1, Spalte 2 → 6
+
+# Flaches Kopieren einer 2D-Liste
+flat = [val for row in grid for val in row]
+
+# Matrix-Operationen (z.B. Transponieren)
+transpose = list(zip(*grid))  # [(1,4,7),(2,5,8),(3,6,9)]
+
+
+
+
+"""
+ ----- Beispiele: JSON-Dateien -----
+"""
 jsonresponse_list = [
     {
         "id": 1,
@@ -104,28 +198,3 @@ print(f"Vereinheitlicht [List]: {vereinheitlicht_list}")
 
 vereinheitlicht_dict = {eintrag["name"]: eintrag for eintrag in vereinheitlicht_list}    #eintrag["name"] = Key (z.B.: Anna)  & eintrag selbst ist das Value (z.B.: {'name': 'Anna'})
 print(f"Vereinheitlicht [Dict]: {vereinheitlicht_dict}")
-
-
-# SET                                                                          #Verwendbar für Mengenoperationen (union, intersection, difference), Duplikat-Erkennung oder -Entfernung, enthält keine Duplikate
-set1 = set()
-my_set = {1, 2, 3, 2}                                                           #the position of the elements can vary
-print(my_set)
-my_set.add(5)
-my_set.remove(5)
-my_set.discard(4)                                                               #Entfernt ein Element (Kein Fehler bei Nichtvorhandensein)
-my_set.pop()                                                                    #Entfernt ein beliebiges Element
-my_set.clear()                                                                  #Löscht alle Elemente
-
-
-#2D List
-number_grid = [
-    [1, 2, 3],                                                                  #4 Elements in a List which are all elements themselves
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
-]
-print(number_grid[0][0])                                                        #How to access the Elements in this kind of list
-for line in number_grid:
-    for col in line:
-        print(col)
-
