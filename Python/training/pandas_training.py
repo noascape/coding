b939d55f51cfc5cd20e6864dfa732e552187594d
paheimno <noah.heim@de.ebmpapst.com>
2025-06-05 11:40:38 +0200

    pandas & path training

--- Python/training/pandas_training.py
+++ Python/training/pandas_training.py
@@ -1,8 +1,31 @@
 #Pandas-Bibliothek: verwendet C-Erweiterungen (meist in Cyphon geschrieben), um bestimmte Operationen zu beschleunigen
 # Dokumentation aller Funktionen: https://pandas.pydata.org/docs/reference/
 
+import os
 import pandas as pd
 
+pandas_version = pd.__version__             #Versions-Check bevor neue Funktionen benutzt werden sollte
+print(pandas_version)
+
+'''
+---- Read .xlsx ----
+'''
+path_xlsx = os.path.join(os.getcwd(), "data", "customer.xlsx")
+df_xlsx = pd.read_excel(path_xlsx)
+print(df_xlsx)
 
 
-df = pd.DataFrame()
+
+'''
+---- Read .xml ----
+'''
+path = os.path.join(os.getcwd(), "data", "books.xml")
+df_xml = pd.read_xml(path, parser="etree")
+print(df_xml)
+
+
+
+'''
+---- Wichtige Funktionen und Möglichkeiten ----
+'''
+#merged_df = pd.merge(df_xlsx, df_xml, on=["Author","Name"], how="outer")
--- Python/training/path_training.py
+++ Python/training/path_training.py
@@ -1,6 +1,41 @@
 import os
+import tempfile
 from pathlib import Path
 
-files_dir = Path.cwd() / "files"
 
-print(files_dir)
+'''
+---------- Relative Pfade -----------
+'''
+print(f"Aktuelles Arbeitsverzeichnis: {os.getcwd()}")               #liefert das Verzeichnis, von dem aus das Skript aufgerufen wurde
+base_dir = os.path.dirname(os.path.abspath(__file__))               #Ordner, unabängig vom Arbeitsverzeichnis
+
+relative_path0 = os.path.join(base_dir, "data", "input.txt")
+relative_path1 = base_dir / "data" / "input.txt"
+print(relative_path0, relative_path1)
+
+
+
+'''
+---------- Absolute Pfade -----------
+'''
+absolute_path_lin = "/home/benutzer/projekt/datei.txt"              #Linux/MacOs
+absolute_path_win = "C:\\Users\\Benutzer\\projekte\\datei.txt"      #Windows
+
+# Aus einem relativen Pfad einen absoluten Pfad erstellen
+absolute_path0 = os.path.abspath("unterordner/datei.txt")           #Absolute Pfade uterscheiden sich je nach Betriebssystem. Am besten mit os.path.join / pathlib arbeiten --> Trennzeichen-Portabilität sicherstellen
+
+
+
+
+'''
+---------- Wichtige Funktionen -----------
+'''
+other_dir = "C:\\Home"
+os.chdir(other_dir)                         #Arbeitsverzeichnis ändern
+home_dir0 = os.path.expanduser("~")         #Home-Verzeichnis des Users
+home_dir1 = Path.home()                     #Home-Verzeichnis des Users
+temp_dir = tempfile.gettempdir()            #Temporäre Verzeichnisse
+
+
+#path.is_file()                             #nochmal prüfen
+#path.exists()
